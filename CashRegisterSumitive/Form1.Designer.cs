80733fcf97630ef577b411973b6990902846140e
AndrHend062 <andrhend062@ed.amdsb.ca>
2017-10-11 13:20:16 -0400

    Add project files.

---
+++ CashRegisterSumitive/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace CashRegisterSumitive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.burgerNumber = new System.Windows.Forms.Label();
+            this.friesLabel = new System.Windows.Forms.Label();
+            this.drinksLabel = new System.Windows.Forms.Label();
+            this.textBoxBurger = new System.Windows.Forms.TextBox();
+            this.textBoxfries = new System.Windows.Forms.TextBox();
+            this.textBoxdrinks = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.totalLable = new System.Windows.Forms.Label();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.textBoxChange = new System.Windows.Forms.TextBox();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // burgerNumber
+            // 
+            this.burgerNumber.Location = new System.Drawing.Point(13, 35);
+            this.burgerNumber.Name = "burgerNumber";
+            this.burgerNumber.Size = new System.Drawing.Size(105, 25);
+            this.burgerNumber.TabIndex = 0;
+            this.burgerNumber.Text = "Number of burgers ";
+            // 
+            // friesLabel
+            // 
+            this.friesLabel.Location = new System.Drawing.Point(13, 70);
+            this.friesLabel.Name = "friesLabel";
+            this.friesLabel.Size = new System.Drawing.Size(105, 25);
+            this.friesLabel.TabIndex = 1;
+            this.friesLabel.Text = "Number of fries";
+            // 
+            // drinksLabel
+            // 
+            this.drinksLabel.Location = new System.Drawing.Point(13, 100);
+            this.drinksLabel.Name = "drinksLabel";
+            this.drinksLabel.Size = new System.Drawing.Size(105, 25);
+            this.drinksLabel.TabIndex = 2;
+            this.drinksLabel.Text = "Number of drinks";
+            // 
+            // textBoxBurger
+            // 
+            this.textBoxBurger.Location = new System.Drawing.Point(124, 35);
+            this.textBoxBurger.Name = "textBoxBurger";
+            this.textBoxBurger.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBurger.TabIndex = 3;
+            this.textBoxBurger.Text = "0";
+            // 
+            // textBoxfries
+            // 
+            this.textBoxfries.Location = new System.Drawing.Point(124, 70);
+            this.textBoxfries.Name = "textBoxfries";
+            this.textBoxfries.Size = new System.Drawing.Size(100, 20);
+            this.textBoxfries.TabIndex = 4;
+            this.textBoxfries.Text = "0";
+            // 
+            // textBoxdrinks
+            // 
+            this.textBoxdrinks.Location = new System.Drawing.Point(124, 100);
+            this.textBoxdrinks.Name = "textBoxdrinks";
+            this.textBoxdrinks.Size = new System.Drawing.Size(100, 20);
+            this.textBoxdrinks.TabIndex = 5;
+            this.textBoxdrinks.Text = "0";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(124, 137);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(100, 23);
+            this.calculateButton.TabIndex = 6;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // totalLable
+            // 
+            this.totalLable.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.totalLable.Location = new System.Drawing.Point(16, 175);
+            this.totalLable.Name = "totalLable";
+            this.totalLable.Size = new System.Drawing.Size(208, 67);
+            this.totalLable.TabIndex = 7;
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.headerLabel.Location = new System.Drawing.Point(-2, -2);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(479, 34);
+            this.headerLabel.TabIndex = 8;
+            this.headerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // changeButton
+            // 
+            this.changeButton.Location = new System.Drawing.Point(124, 271);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(100, 23);
+            this.changeButton.TabIndex = 9;
+            this.changeButton.Text = "Change ";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // textBoxChange
+            // 
+            this.textBoxChange.Location = new System.Drawing.Point(124, 245);
+            this.textBoxChange.Name = "textBoxChange";
+            this.textBoxChange.Size = new System.Drawing.Size(100, 20);
+            this.textBoxChange.TabIndex = 10;
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.Location = new System.Drawing.Point(124, 322);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(100, 23);
+            this.changeLabel.TabIndex = 11;
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.Location = new System.Drawing.Point(127, 349);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(97, 23);
+            this.receiptButton.TabIndex = 12;
+            this.receiptButton.Text = "Print";
+            this.receiptButton.UseVisualStyleBackColor = true;
+            this.receiptButton.Click += new System.EventHandler(this.receiptButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 371);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.textBoxChange);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.headerLabel);
+            this.Controls.Add(this.totalLable);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.textBoxdrinks);
+            this.Controls.Add(this.textBoxfries);
+            this.Controls.Add(this.textBoxBurger);
+            this.Controls.Add(this.drinksLabel);
+            this.Controls.Add(this.friesLabel);
+            this.Controls.Add(this.burgerNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label burgerNumber;
+        private System.Windows.Forms.Label friesLabel;
+        private System.Windows.Forms.Label drinksLabel;
+        private System.Windows.Forms.TextBox textBoxBurger;
+        private System.Windows.Forms.TextBox textBoxfries;
+        private System.Windows.Forms.TextBox textBoxdrinks;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label totalLable;
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.TextBox textBoxChange;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Button receiptButton;
+    }
+}
+
---
+++ CashRegisterSumitive/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashRegisterSumitive
+{
+    public partial class Form1 : Form
+    {
+        const double BURGER_PRICE = 2.49;
+        const double FRIES_PRICE = 1.89;
+        const double DRINK_PRICE = 0.99;
+        double burgers;
+        double fries;
+        double drinks;
+        double burgersCost;
+        double friesCost;
+        double drinksCost;
+        double Tax = 0.13;
+        double costBFD;
+        double taxPrice;
+        double totalPrice;
+        double changePay;
+        double changeDue;
+
+
+        public Form1()
+
+
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                burgers = Convert.ToInt32(textBoxBurger.Text);
+                fries = Convert.ToInt32(textBoxfries.Text);
+                drinks = Convert.ToInt32(textBoxdrinks.Text);
+
+                friesCost = fries * FRIES_PRICE;
+                burgersCost = burgers * BURGER_PRICE;
+                drinksCost = drinks * DRINK_PRICE;
+                costBFD = burgersCost + drinksCost + friesCost;
+                taxPrice = costBFD * Tax;
+                totalPrice = taxPrice + costBFD;
+
+
+                totalLable.Text = "sub total      " + costBFD +
+                    "\n tax       " + taxPrice +
+                    "\n total    " + totalPrice;
+
+
+            }
+            catch
+            {
+                totalLable.Text = "enter a whole number";
+            }
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+
+            friesCost = fries * FRIES_PRICE;
+            burgersCost = burgers * BURGER_PRICE;
+            drinksCost = drinks * DRINK_PRICE;
+            costBFD = burgersCost + drinksCost + friesCost;
+            taxPrice = costBFD * Tax;
+            totalPrice = taxPrice + costBFD;
+
+            changePay = Convert.ToInt32(textBoxChange.Text);
+             
+            changeDue = changePay - totalPrice;
+
+            changeLabel.Text = "Change due " + changeDue.ToString("C");
+        }
+
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Graphics fG = this.CreateGraphics();
+                SolidBrush whiteBrush = new SolidBrush(Color.White);
+                SolidBrush blackBrush = new SolidBrush(Color.Black);
+                Pen penPen = new Pen(Color.Red, 10);
+                Font arialFont = new Font("Arial", 10, FontStyle.Regular);
+
+
+                fG.FillRectangle(whiteBrush, 300, 50, 150, 300);
+                fG.DrawString("        receipt" + "\n"+
+                    "Burgers  X" + burgers + "\n" +
+                    "Fries    X" + fries + "\n"+
+                    "Drinks   X" +drinks + "\n", arialFont, blackBrush, 300, 60);
+                fG.DrawString(  burgersCost + "\n" +
+                    friesCost + "\n"+ 
+                    drinksCost + "\n", arialFont, blackBrush, 380, 60);
+
+
+            }
+            catch
+            {
+                totalLable.Text = "error";
+            }
+        }
+    }
+}
